2f9f3ab15ced824a94b10d904dce03bf7e1492fb
calvinkrill <calv.dc21@gmail.com>
2026-04-25 00:27:19 +0800

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,33 @@
+import discord
+from discord.ext import commands
+import os
+from dotenv import load_dotenv
+
+# Load environment variables
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+# Initialize bot with all intents
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user.name} (ID: {bot.user.id})')
+    print('------')
+
+@bot.command()
+async def ping(ctx):
+    """Responds with Pong!"""
+    await ctx.send('Pong!')
+
+@bot.command()
+async def echo(ctx, *, message):
+    """Echoes back the message provided."""
+    await ctx.send(message)
+
+if __name__ == '__main__':
+    if not TOKEN:
+        print("Error: DISCORD_TOKEN not found in .env file.")
+    else:
+        bot.run(TOKEN)
